12bf1fc10b3b46aac1f256b5d3c3161b88f9ccb6
shashankmangal10@gmail.com <Sunil@1234>
2022-12-12 17:31:21 +0530

    main screen

---
+++ app/src/main/java/com/scammer101/Virya/Screens/MainScreen.kt
@@ -0,0 +1,12 @@
+package com.scammer101.Virya.Screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.scammer101.Virya.R
+
+class MainScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
