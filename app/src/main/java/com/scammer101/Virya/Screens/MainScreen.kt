4f9e8eec1e055d7d2b883f09f114bb78405dcb43
Shashank Mangal <shashankmangal10@gmail.com>
2022-12-13 11:28:03 +0530

    meow navigation removed (NOT WORKING)

--- app/src/main/java/com/scammer101/Virya/Screens/MainScreen.kt
+++ app/src/main/java/com/scammer101/Virya/Screens/MainScreen.kt
@@ -2,11 +2,25 @@ package com.scammer101.Virya.Screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.scammer101.Virya.R
+import com.scammer101.Virya.databinding.ActivityMainScreenBinding
 
 class MainScreen : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_screen)
+        binding = ActivityMainScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
+
+
+
+
 }
