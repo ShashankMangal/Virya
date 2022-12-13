56ddbb3d3ba59d406da4100cdca05fc25dbdfd4c
shashankmangal10@gmail.com <Sunil@1234>
2022-12-13 13:05:54 +0530

    chip navigation added

--- app/src/main/java/com/scammer101/Virya/Screens/MainScreen.kt
+++ app/src/main/java/com/scammer101/Virya/Screens/MainScreen.kt
@@ -1,7 +1,10 @@
 package com.scammer101.Virya.Screens
 
+import android.app.Activity
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.scammer101.Virya.R
@@ -9,18 +12,43 @@ import com.scammer101.Virya.databinding.ActivityMainScreenBinding
 
 class MainScreen : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainScreenBinding
+    private lateinit var binding: ActivityMainScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        init()
 
     }
 
 
+    private fun init() {
 
+        setStatusBarColor(Color.parseColor("#EAFDDD"))
+        binding.bottomNavigation.setItemEnabled(R.id.home, true)
+        binding.bottomNavigation.showBadge(R.id.home)
+        binding.bottomNavigation.showBadge(R.id.goals, 10)
+    }
 
+    fun Activity.setStatusBarColor(color: Int) {
+        var flags = window?.decorView?.systemUiVisibility // get current flag
+        if (flags != null) {
+            if (isColorDark(color)) {
+                flags = flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+                window?.decorView?.systemUiVisibility = flags
+            } else {
+                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                window?.decorView?.systemUiVisibility = flags
+            }
+        }
+        window?.statusBarColor = color
+    }
+
+    fun Activity.isColorDark(color: Int): Boolean {
+        val darkness =
+            1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255
+        return darkness >= 0.5
+    }
 }
