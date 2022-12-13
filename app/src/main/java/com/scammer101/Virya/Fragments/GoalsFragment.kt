d6ecf5aae18259df034ba51e04409eaabe616cbc
shashankmangal10@gmail.com <Sunil@1234>
2022-12-13 20:14:23 +0530

    custom progress added in goals fragment

--- app/src/main/java/com/scammer101/Virya/Fragments/GoalsFragment.kt
+++ app/src/main/java/com/scammer101/Virya/Fragments/GoalsFragment.kt
@@ -1,22 +1,36 @@
 package com.scammer101.Virya.Fragments
 
+import android.animation.ObjectAnimator
+import android.app.ProgressDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.scammer101.Virya.R
+import com.scammer101.Virya.databinding.FragmentGoalsBinding
 
 class GoalsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentGoalsBinding? = null
+    private val binding get() = _binding!!
+    private var currentProgress : Int = 0
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goals, container, false)
+        _binding = FragmentGoalsBinding.inflate(inflater, container, false)
+
+        init()
+
+        return binding.root
+    }
+
+    private fun init()
+    {
+        currentProgress = 80
+        ObjectAnimator.ofInt(binding.goalsProgressBar, "progress", currentProgress).setDuration(2000).start()
     }
 
 }
