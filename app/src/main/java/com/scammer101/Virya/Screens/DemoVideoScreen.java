a9ebe569267dd537d33e88be36473d17b2976f64
shashankmangal10@gmail.com <Sunil@1234>
2022-12-15 17:01:15 +0530

    Video Demo Player Done

---
+++ app/src/main/java/com/scammer101/Virya/Screens/DemoVideoScreen.java
@@ -0,0 +1,57 @@
+package com.scammer101.Virya.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.scammer101.Virya.databinding.ActivityDemoVideoScreenBinding;
+
+public class DemoVideoScreen extends AppCompatActivity {
+
+    private ActivityDemoVideoScreenBinding binding;
+    private ExoPlayer exoPlayer;
+    private String url;
+    private String yogaPose;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDemoVideoScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent i = getIntent();
+        yogaPose = i.getStringExtra("yoga");
+        init();
+        setListener();
+
+    }
+
+    private void init()
+    {
+        exoPlayer = new ExoPlayer.Builder(this).build();
+        url = "https://firebasestorage.googleapis.com/v0/b/virya-50134.appspot.com/o/Yoga%2Ftreepose.mp4?alt=media&token=a9059645-257c-4b79-b332-23ff7510038a";
+    }
+
+    private void setListener()
+    {
+        binding.demoExoPlayer.setPlayer(exoPlayer);
+        MediaItem mediaItem = MediaItem.fromUri(url);
+        exoPlayer.addMediaItem(mediaItem);
+        exoPlayer.prepare();
+
+        binding.demoNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DemoVideoScreen.this, PoseDetectorActivity.class);
+                intent.putExtra("yoga", yogaPose);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
